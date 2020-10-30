679158832c00c1f373d0c86e7bbccfcdb3aaca88
ccje31 <chelsea.chan@kingdomdigital.com.my>
2020-10-30 16:05:07 +0800

    Expanded to match pose array, shoulder keypoints

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-let capture;
-let poseNet;
-let pose;
-let skeleton;
-let xToMatch = 0;
-let yToMatch = 0;
+let capture, poseNet, pose, skeleton;
+let poseToMatch = [];
 let dotColor, defaultColor, correctColor, skeletonColor;
-const btn = document.getElementById('record'), chunks = [];
+const recordBtn = document.getElementById('record'), chunks = [];
 
 function record() {
   chunks.length = 0;
@@ -17,13 +13,13 @@ function record() {
     }
   };
   recorder.onstop = exportVideo;
-  btn.onclick = e => {
+  recordBtn.onclick = e => {
     recorder.stop();
-    btn.textContent = 'start recording';
-    btn.onclick = record;
+    recordBtn.textContent = 'start recording';
+    recordBtn.onclick = record;
   };
   recorder.start();
-  btn.textContent = 'stop recording';
+  recordBtn.textContent = 'stop recording';
 }
 
 function exportVideo(e) {
@@ -37,7 +33,7 @@ function exportVideo(e) {
   document.body.appendChild(vid);
   vid.play();
 }
-btn.onclick = record;
+recordBtn.onclick = record;
 
 function setup() {
   createCanvas(320, 240);
@@ -50,14 +46,14 @@ function setup() {
 
   xToMatch = width/2;
   yToMatch = height/2;
-  defaultColor = color('#ff414d');
-  correctColor = color('#28df99');
-  skeletonColor = color('#a5ecd7');
+  defaultColor = color(255,65,10,70);
+  correctColor = color(0,255,0,255);
+  skeletonColor = color(50,168, 145,100);
   dotColor = defaultColor;
 }
 
 function gotPoses(poses) {
-  // console.log(poses);
+  console.log(poses);
 
   if (poses.length > 0){
     pose = poses[0].pose;
@@ -69,18 +65,46 @@ function modelReady() {
   console.log('poseNet model ready');
 }
 
-function drawRandomPose(x,y,clr){
-  noStroke();
-  fill(clr);
-  ellipse(x,y,18,18);
+function drawRandomPose(colr){
+  poseToMatch = [
+    [0,0],//nose
+    [0,0],//lefteye
+    [0,0],//righteye
+    [0,0],//leftear
+    [0,0],//rigtear
+    [244,170],//leftshoulder
+    [80,170],//rightshoulder
+    [0,0],//leftelbow
+    [0,0],//rightelbow
+    [0,0],//leftwrist
+    [0,0],//rightwrist
+    [0,0],//lefthip
+    [0,0],//righthip
+    [0,0],//leftknee
+    [0,0],//rightknee
+    [0,0],//leftankle
+    [0,0],//rightankle
+  ];
+  for (let i=0; i<poseToMatch.length; i++) {
+    noStroke();
+    fill(colr);
+    ellipse(poseToMatch[i][0],poseToMatch[i][1],15,15);
+  }
+
+  strokeWeight(20);
+  stroke(colr);
+  line(poseToMatch[5][0], poseToMatch[5][1],poseToMatch[6][0],poseToMatch[6][1]);
+
 }
 
 function matchPose(x1,y1,x2,y2){
   let d = dist(x1,y1,x2,y2);
-  if (d < 10){
+  if (d < 20){
     dotColor = correctColor;
+    console.log('match');
   } else {
     dotColor = defaultColor;
+    console.log('no match');
   }
 }
 
@@ -96,21 +120,21 @@ function draw() {
       let y = pose.keypoints[i].position.y;
       noStroke();
       fill(skeletonColor);
-      ellipse(x,y,16,16);
+      ellipse(x,y,15,15);
 
-      if (i == 0){
-        matchPose(x,y,xToMatch,yToMatch);
+      if (i==5 || i==6) {
+        matchPose(x,y,poseToMatch[i][0],poseToMatch[i][1]);
       }
     }
 
     for (let i = 0; i < skeleton.length; i++){
       let a = skeleton[i][0];
       let b = skeleton[i][1];
-      strokeWeight(2);
+      strokeWeight(20);
       stroke(skeletonColor);
       line(a.position.x, a.position.y, b.position.x, b.position.y);
     }
 
   }
-  drawRandomPose(xToMatch, yToMatch, dotColor);
+  drawRandomPose(dotColor);
 }
